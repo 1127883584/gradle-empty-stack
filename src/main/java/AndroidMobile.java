aa7d0ef5983ffb72fedc386a7638f675e6e8517e
1127883584@qq.com <1127883584@qq.com>
2019-07-11 14:40:41 +0800

    finish story1

---
+++ src/main/java/AndroidMobile.java
@@ -0,0 +1,5 @@
+public class AndroidMobile extends Mobile {
+    public void makeCall(String message) {
+        System.out.println("<Andriod>Message : " + message);
+    }
+}
---
+++ src/main/java/Iphone.java
@@ -0,0 +1,5 @@
+public class Iphone extends Mobile{
+    public void makeCall(String message){
+        System.out.println("<iPhone>Message : " + message);
+    }
+}
